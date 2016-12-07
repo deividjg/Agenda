1ff17054cdc99d6683d99d6a86f2b2333856282a
deividjg <deividjg@gmail.com>
2016-12-07 19:48:25 +0100

    añadidas preferencias

---
+++ app/src/main/java/com/example/david/agenda/Preferencias.java
@@ -0,0 +1,17 @@
+package com.example.david.agenda;
+
+import android.preference.PreferenceActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Preferencias extends PreferenceActivity {
+
+    @SuppressWarnings("deprecation")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_preferencias);
+        addPreferencesFromResource(R.xml.preferencias);
+
+    }
+}
